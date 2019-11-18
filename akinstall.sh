b889062bfdd47ae47d8e8030a85a697f4bd4620b
haruka98 <45176998+haruka98@users.noreply.github.com>
2019-11-18 08:22:19 +0100

    user input for folder creation and ip selection

--- akinstall.sh
+++ akinstall.sh
@@ -23,3 +23,29 @@ sed -i "s+host    all             all             127.0.0.1/32            md5+ho
 
 # change password for the postgres account
 sudo -u postgres psql -c "ALTER user postgres WITH password '$DBPASS';"
+
+# test for the main folder
+if [ -d "/root/hxsy" ] ; then
+	echo "The folder /root/hxsy already exists, please rename or delete it before running the script."
+	echo "Delete existing folder? (y/n)"
+	read INVAR
+	if [ "$INVAR" != "y" ] && [ "$INVAR" != "Y" ] ; then
+		exit
+	fi
+	rm -rf "/root/hxsy"
+fi
+mkdir "/root/hxsy" -m 777
+cd "/root/hxsy"
+
+# get ip info; select ip
+EXTIP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+if [ "$EXTIP" != "" ] ; then
+	echo "Select your IP:\n1) External IP: $EXTIP\n2) Input other IP"
+	read INVAR
+else
+	INVAR="2"
+fi
+if [ "$INVAR" = "2" ] ; then
+	echo "Please enter IP:"
+	read EXTIP
+fi
