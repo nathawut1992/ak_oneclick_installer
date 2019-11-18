4effbc04f8ad243e70cd3824dbd545d939f7f5b3
haruka98 <45176998+haruka98@users.noreply.github.com>
2019-11-18 08:13:57 +0100

    setup postgresql and update database password

--- akinstall.sh
+++ akinstall.sh
@@ -2,8 +2,24 @@
 
 # make sure lists are up to date
 apt-get update
+
 # install wget in case it is missing
 sudo apt-get install wget -y
+
 # install postgresql in case it is missing
 apt-get install postgresql -y
 POSTGRESQLVERSION=$(psql --version | grep -Eo '[0-9].[0-9]')
+
+# install pwgen in case it is missing
+apt-get install pwget -y
+
+# generate database password
+DBPASS=$(pwgen -s 32 1)
+
+# setup postgresql
+cd "/etc/postgresql/$POSTGRESQLVERSION/main"
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" postgresql.conf
+sed -i "s+host    all             all             127.0.0.1/32            md5+host    all             all             0.0.0.0/0            md5+g" pg_hba.conf
+
+# change password for the postgres account
+sudo -u postgres psql -c "ALTER user postgres WITH password '$DBPASS';"
