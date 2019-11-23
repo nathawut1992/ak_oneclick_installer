ca0c5119419ef14f9145e8bd836093b9d13b49fe
Wolf Gp <45176998+haruka98@users.noreply.github.com>
2019-11-23 11:16:47 +0100

    added yokohiro 003.005.01.05

--- akinstall.sh
+++ akinstall.sh
@@ -34,7 +34,7 @@ if [ "$INVAR" = "2" ] ; then
 fi
 
 # select server version
-echo "Select the version you want to install.\n1) genz - 003.005.01.04"
+echo "Select the version you want to install.\n1) yokohiro - 003.005.01.05\n2) genz - 003.005.01.04"
 read AKVERSION
 
 # make sure lists are up to date
@@ -79,7 +79,7 @@ VERSIONNAME="NONE"
 # --------------------------------------------------
 # genz - 003.005.01.04
 # --------------------------------------------------
-if [ "$AKVERSION" = 1 ] ; then
+if [ "$AKVERSION" = 2 ] ; then
 	cd "/root/hxsy"
 	wget --no-check-certificate "https://raw.githubusercontent.com/haruka98/ak_oneclick_installer/master/genz_003_005_01_04" -O "genz_003_005_01_04"
 	chmod 777 genz_003_005_01_04
@@ -138,6 +138,72 @@ if [ "$AKVERSION" = 1 ] ; then
 	# setup info
 	VERSIONNAME="genz - 003.005.01.04"
 	CREDITS="genz and Eperty123"
+fi
+
+# --------------------------------------------------
+# yokohiro - 003.005.01.05
+# --------------------------------------------------
+if [ "$AKVERSION" = 1 ] ; then
+	cd "/root/hxsy"
+	wget --no-check-certificate "https://raw.githubusercontent.com/haruka98/ak_oneclick_installer/master/yokohiro_003_005_01_05" -O "yokohiro_003_005_01_05"
+	chmod 777 yokohiro_003_005_01_05
+	. "/root/hxsy/yokohiro_003_005_01_05"
+	
+	# config files
+	wget --no-check-certificate "$MAINCONFIG" -O "config.zip"
+	unzip "config.zip"
+	rm -f "config.zip"
+	sed -i "s/123456/$DBPASS/g" "setup.ini"
+	
+	# subservers
+	wget --no-check-certificate --load-cookies "/tmp/cookies.txt" "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$SUBSERVERSID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$SUBSERVERSID" -O "server.zip" && rm -rf "/tmp/cookies.txt"
+	unzip "server.zip"
+	rm -f "server.zip"
+	sed -i "s/192.168.0.33/$EXTIP/g" "GatewayServer/setup.ini"
+	sed -i "s/123456/$DBPASS/g" "GatewayServer/setup.ini"
+	sed -i "s/192.168.0.33/$EXTIP/g" "TicketServer/setup.ini"
+	sed -i "s/\xff\x3d\xc0\xa8\x00/\xff\x3d$PATCHIP/g" "WorldServer101/WorldServer101"
+	sed -i "s/\xff\x3d\xc0\xa8\x00/\xff\x3d$PATCHIP/g" "WorldServer102/WorldServer102"
+	sed -i "s/\xff\x3d\xc0\xa8\x00/\xff\x3d$PATCHIP/g" "ZoneServer101/ZoneServer101"
+	sed -i "s/\xff\x3d\xc0\xa8\x00/\xff\x3d$PATCHIP/g" "ZoneServer102/ZoneServer102"
+	
+	# Data folder
+	wget --no-check-certificate "$DATAFOLDER" -O "Data.zip"
+	unzip "Data.zip" -d "Data"
+	rm -f "Data.zip"
+	
+	# SQL files
+	wget --no-check-certificate "$SQLFILES" -O "SQL.zip"
+	unzip "SQL.zip" -d "SQL"
+	rm -f "SQL.zip"
+	
+	# set permissions
+	chmod 777 /root -R
+	
+	# install postgresql database
+	service postgresql restart
+	sudo -u postgres psql -c "create database ffaccount encoding 'UTF8' template template0;"
+	sudo -u postgres psql -c "create database ffdb1 encoding 'UTF8' template template0;"
+	sudo -u postgres psql -c "create database ffmember encoding 'UTF8' template template0;"
+	sudo -u postgres psql -c "create database itemmall encoding 'UTF8' template template0;"
+	sudo -u postgres psql -d ffaccount -c "\i '/root/hxsy/SQL/FFAccount.sql';"
+	sudo -u postgres psql -d ffdb1 -c "\i '/root/hxsy/SQL/FFDB1.sql';"
+	sudo -u postgres psql -d ffmember -c "\i '/root/hxsy/SQL/FFMember.sql';"
+	sudo -u postgres psql -d itemmall -c "\i '/root/hxsy/SQL/Itemmall.sql';"
+	sudo -u postgres psql -d ffaccount -c "UPDATE worlds SET ip = '$EXTIP' WHERE ip = '192.168.198.129';"
+	sudo -u postgres psql -d ffdb1 -c "UPDATE serverstatus SET ext_address = '$EXTIP' WHERE ext_address = '192.168.198.129';"
+	
+	# remove server setup files
+	rm -f yokohiro_003_005_01_05
+	
+	#set the server date to 2013
+	timedatectl set-ntp 0
+	date -s "$(date +'2013%m%d %H:%M')"
+	hwclock --systohc
+	
+	# setup info
+	VERSIONNAME="yokohiro - 003.005.01.05"
+	CREDITS="yokohiro, genz and Eperty123"
 fi
 
 if [ "$VERSIONNAME" = "NONE" ] ; then
